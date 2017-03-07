eed46512fa0692038f9716978c8f25a80ee77653
Niten <niten2@yandex.ru>
2017-03-07 17:40:53 +0700

    change docs

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -31,7 +31,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     publicPath: '/',
     // publicPath: ASSETS_HOST,
     filename: '[name].[chunkhash].js'
