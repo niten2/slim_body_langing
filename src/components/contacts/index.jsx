5a37295490260ef92ef006ae302c64046f390010
Niten <niten2@yandex.ru>
2017-03-23 22:50:47 +0700

    deploy

--- src/components/contacts/index.jsx
+++ src/components/contacts/index.jsx
@@ -6,7 +6,7 @@ export default class Contacts extends Component {
   componentDidMount() {
     if (!DEV) {
       const script = document.createElement("script")
-      script.src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=wG6Fe7o8YH3RQ8vsuFDXimjPPmOgYFYZ&amp;width=100%&amp;height=500&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=true"
+      script.src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=wG6Fe7o8YH3RQ8vsuFDXimjPPmOgYFYZ&amp;width=100%&amp;height=80%&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=true"
       script.async = true
       this.refs.script.appendChild(script)
     }
