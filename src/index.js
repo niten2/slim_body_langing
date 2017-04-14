ae38dc50219b580171de29ec9f9096f328a8ccec
Niten <niten2@yandex.ru>
2017-04-14 23:39:19 +0700

    edit styles

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import { render } from 'react-dom'
 import routes from './routes'
 import 'styles/index.scss'
 
-// global values
-// import 'lib/callback_phone'
-// if (!DEV) {
-//   require("lib/vk")
-// }
-
 render(
   routes(),
   document.getElementById('app')
