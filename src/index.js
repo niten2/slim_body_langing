a7cf33d9e16e0eb8412df1bffdb5c93e3572919c
Niten <niten2@yandex.ru>
2017-04-15 10:18:30 +0700

    change index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { render } from 'react-dom'
 import routes from './routes'
 import 'styles/index.scss'
 
+// document.getElementById("app").remove()
+// var test = document.createElement('div')
+// test.id = 'app'
+// document.body.appendChild(test)
+
 render(
   routes(),
   document.getElementById('app')
